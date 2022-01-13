fa626ff16e04e9317603aed65a632ea545a28bf3
roy <ykh13571@naver.com>
2022-01-13 13:31:14 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,15 @@
+print("백문이 붙여일견\n백견이 붙여일타")
+
+print("저는 \'ajaj\'입니다.")
+print("저는 \'j\'입니다.")
+#\\: 문장내에서는 \
+print("C:\\Users\\PC\\Desktop\\pythonWorkspace>")
+
+#\r: 커서를 맨 앞으로 이동
+print("Red Apple\rPine") #\r이 Red 앞으로 이동해서 Pine글자를 씀
+
+#\b: 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+#\t: 탭
+print("Red\tApple")
